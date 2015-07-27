d0efc74ca0f0e51a046ed7440a9450f77b0402ce
Marin Atanasov <tyxla@abv.bg>
2015-07-28 00:26:01 +0300

    bump version in plugin

--- gravityforms-multiple-form-instances.php
+++ gravityforms-multiple-form-instances.php
@@ -5,10 +5,10 @@
  * Description: Allows multiple instances of the same form to be run on a single page when using AJAX.
  * Author: tyxla
  * Author URI: https://github.com/tyxla
- * Version: 1.0.5
+ * Version: 1.0.6
  * License: GPL2
  * Requires at least: 3.0.1
- * Tested up to: 4.2.2
+ * Tested up to: 4.2.3
  */
 
 /**
