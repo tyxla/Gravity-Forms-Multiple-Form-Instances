46bb01a1134705e2dd653a6e2a16d94ac580741b
Marin Atanasov <tyxla@abv.bg>
2016-02-04 23:36:32 +0200

    done tests for Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter()

--- tests/unit-tests/GFMFI_GformGetFormFilterTest.php
+++ tests/unit-tests/GFMFI_GformGetFormFilterTest.php
@@ -228,7 +228,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGformInitSpinner() {
+	public function testGformInitSpinnerReplacement() {
 		$input = 'gformInitSpinner( ' . $this->form['id'] . ',';
 		$expected = 'gformInitSpinner( ' . $this->randomId . ',';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -250,7 +250,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGformConfirmationLoaded() {
+	public function testGformConfirmationLoadedReplacement() {
 		$input = "'gform_confirmation_loaded', [" . $this->form['id'] . ']';
 		$expected = "'gform_confirmation_loaded', [" . $this->randomId . ']';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -261,7 +261,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGfApplyRules() {
+	public function testGfApplyRulesReplacement() {
 		$input = 'gf_apply_rules(' . $this->form['id'] . ',';
 		$expected = 'gf_apply_rules(' . $this->randomId . ',';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -272,7 +272,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGformConfirmationWrapper() {
+	public function testGformConfirmationWrapperReplacement() {
 		$input = 'gform_confirmation_wrapper_' . $this->form['id'];
 		$expected = 'gform_confirmation_wrapper_' . $this->randomId;
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -283,7 +283,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGformsConfirmationMessage() {
+	public function testGformsConfirmationMessageReplacement() {
 		$input = 'gforms_confirmation_message_' . $this->form['id'];
 		$expected = 'gforms_confirmation_message_' . $this->randomId;
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -294,7 +294,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGformConfirmationMessage() {
+	public function testGformConfirmationMessageReplacement() {
 		$input = 'gform_confirmation_message_' . $this->form['id'];
 		$expected = 'gform_confirmation_message_' . $this->randomId;
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -305,7 +305,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testIfFormId() {
+	public function testIfFormIdReplacement() {
 		$input = 'if(formId == ' . $this->form['id'] . ')';
 		$expected = 'if(formId == ' . $this->randomId . ')';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -316,7 +316,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testWindowGfFormConditionalLogic() {
+	public function testWindowGfFormConditionalLogicReplacement() {
 		$input = "window['gf_form_conditional_logic'][" . $this->form['id'] . ']';
 		$expected = "window['gf_form_conditional_logic'][" . $this->randomId . ']';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -327,7 +327,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testTriggerGformPostConditionalLogic() {
+	public function testTriggerGformPostConditionalLogicReplacement() {
 		$input = "trigger('gform_post_conditional_logic', [" . $this->form['id'] . ',';
 		$expected = "trigger('gform_post_conditional_logic', [" . $this->randomId . ',';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -338,7 +338,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGformShowPasswordStrengthInput() {
+	public function testGformShowPasswordStrengthInputReplacement() {
 		$input = 'gformShowPasswordStrength("input_' . $this->form['id'] . '_';
 		$expected = 'gformShowPasswordStrength("input_' . $this->randomId . '_';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -349,7 +349,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGformInitChosenFieldsInput() {
+	public function testGformInitChosenFieldsInputReplacement() {
 		$input = "gformInitChosenFields('#input_" . $this->form['id'] . '_';
 		$expected = "gformInitChosenFields('#input_" . $this->randomId . '_';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -360,7 +360,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testJqueryHashInput() {
+	public function testJqueryHashInputReplacement() {
 		$input = "jQuery('#input_" . $this->form['id'] . '_';
 		$expected = "jQuery('#input_" . $this->randomId . '_';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -371,7 +371,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGformsCalendarIconInput() {
+	public function testGformsCalendarIconInputReplacement() {
 		$input = 'gforms_calendar_icon_input_' . $this->form['id'] . '_';
 		$expected = 'gforms_calendar_icon_input_' . $this->randomId . '_';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -382,7 +382,18 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testIdGinputQuantity() {
+	public function testIdGinputBasePriceReplacement() {
+		$input = "id='ginput_base_price_" . $this->form['id'] . '_';
+		$expected = "id='ginput_base_price_" . $this->randomId . '_';
+		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
+	 */
+	public function testIdGinputQuantityReplacement() {
 		$input = "id='ginput_quantity_" . $this->form['id'] . '_';
 		$expected = "id='ginput_quantity_" . $this->randomId . '_';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -393,7 +404,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGfieldPrice() {
+	public function testGfieldPriceReplacement() {
 		$input = 'gfield_price_' . $this->form['id'] . '_';
 		$expected = 'gfield_price_' . $this->randomId . '_';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -404,7 +415,7 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGfieldQuantity() {
+	public function testGfieldQuantityReplacement() {
 		$input = 'gfield_quantity_' . $this->form['id'] . '_';
 		$expected = 'gfield_quantity_' . $this->randomId . '_';
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
@@ -415,9 +426,64 @@ class GFMFI_GformGetFormFilterTest extends WP_UnitTestCase {
 	/**
 	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
 	 */
-	public function testGfieldProduct() {
+	public function testGfieldProductReplacement() {
 		$input = 'gfield_product_' . $this->form['id'] . '_';
 		$expected = 'gfield_product_' . $this->randomId . '_';
+		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
+	 */
+	public function testGinputTotalReplacement() {
+		$input = 'ginput_total_' . $this->form['id'];
+		$expected = 'ginput_total_' . $this->randomId;
+		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
+	 */
+	public function testGfCalcReplacement() {
+		$input = 'GFCalc(' . $this->form['id'] . ',';
+		$expected = 'GFCalc(' . $this->randomId . ',';
+		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
+	 */
+	public function testGfGlobalNumberFormatsReplacement() {
+		$input = 'gf_global["number_formats"][' . $this->form['id'] . ']';
+		$expected = 'gf_global["number_formats"][' . $this->randomId . ']';
+		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
+	 */
+	public function testGformNextButtonReplacement() {
+		$input = 'gform_next_button_' . $this->form['id'] . '_';
+		$expected = 'gform_next_button_' . $this->randomId . '_';
+		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	/**
+	 * @covers Gravity_Forms_Multiple_Form_Instances::gform_get_form_filter
+	 */
+	public function testHiddenFieldAddition() {
+		$input = "<input type='hidden' name='gform_field_values'";
+		$expected = "<input type='hidden' name='gform_random_id' value='" . $this->randomId . "' />" . $input;
 		$actual = $this->gfmfi->gform_get_form_filter( $input, $this->form );
 
 		$this->assertSame( $expected, $actual );
